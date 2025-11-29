1927ea8e226c66e22d1e5cb571dae0e07f9f1a9a
kuromeyy <kellenvaleriee@gmail.com>
2025-11-29 17:39:52 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/lab_week_12/MovieRepository.kt
@@ -1,7 +1,33 @@
 package com.example.lab_week_12
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.lab_week_12.api.MovieService
+import com.example.lab_week_12.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
 
+    private val apiKey = "a8fca104c32b480bbd358010a307b716"
+
+    // LiveData list movie
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+
+    // LiveData error
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    // Fetch movies dari API (COROUTINE)
+    suspend fun fetchMovies() {
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (exception: Exception) {
+            errorLiveData.postValue(
+                "An error occurred: ${exception.message}"
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/lab_week_12/model/Movie.kt
+++ app/src/main/java/com/example/lab_week_12/model/Movie.kt
@@ -1,25 +1,41 @@
 package com.example.lab_week_12.model
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class Movie(
+
     val adult: Boolean = false,
-    val backdrop_path: String = "",
+
+    @Json(name = "backdrop_path")
+    val backdropPath: String? = null,
+
     val id: Int = 0,
-    @field:Json(name = "original_language")
-    val originalLanguage: String = "",
-    @field:Json(name = "original_title")
-    val originalTitle: String = "",
-    val overview: String = "",
+
+    @Json(name = "original_language")
+    val originalLanguage: String? = null,
+
+    @Json(name = "original_title")
+    val originalTitle: String? = null,
+
+    val overview: String? = null,
+
     val popularity: Float = 0f,
-    @field:Json(name = "poster_path")
-    val posterPath: String = "",
-    @field:Json(name = "release_date")
-    val releaseDate: String = "",
-    val title: String = "",
+
+    @Json(name = "poster_path")
+    val posterPath: String? = null,
+
+    @Json(name = "release_date")
+    val releaseDate: String? = null,
+
+    val title: String? = null,
+
     val video: Boolean = false,
-    @field:Json(name = "vote_average")
+
+    @Json(name = "vote_average")
     val voteAverage: Float = 0f,
-    @field:Json(name = "vote_count")
+
+    @Json(name = "vote_count")
     val voteCount: Int = 0
 )
